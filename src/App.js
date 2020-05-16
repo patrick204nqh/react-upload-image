a7f604cd59d8c24407481deffac21391a3e26170
Patrick <patrick204nqh@gmail.com>
2020-05-16 15:26:19 +0700

    more info about Cloud Function Firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    selectedFile: null,
+  };
+
+  fileSelectedHandler = (e) => {
+    this.setState({ selectedFile: e.target.files[0] });
+  };
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append(
+      'image',
+      this.setState.selectedFile,
+      this.state.selectedFile.name
+    );
+    axios
+      .post('https://cloud-function', fd, {
+        onUploadProgress: (progressEvent) => {
+          console.log(
+            'Upload Progress ' +
+              Math.round(progressEvent.loaded / progressEvent.total) * 100 +
+              '%'
+          );
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  render() {
+    return (
+      <div className='input-group mb-3'>
+        <div className='custom-file'>
+          <input
+            type='file'
+            className='custom-file-input'
+            id='inputGroupFile02'
+            onChange={this.fileSelectedHandler}
+            ref={(fileInput) => (this.fileInput = fileInput)}
+          />
+          <label
+            className='custom-file-label'
+            htmlFor='inputGroupFile02'
+            aria-describedby='inputGroupFileAddon02'
+            onClick={() => this.fileInput.click()}
+          >
+            Choose file
+          </label>
+        </div>
+        <div className='input-group-append'>
+          <span
+            className='input-group-text'
+            id='inputGroupFileAddon02'
+            onClick={this.fileUploadHandler}
+          >
+            Upload
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
